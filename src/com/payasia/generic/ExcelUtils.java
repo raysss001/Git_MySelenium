5f9369bc8690d4b67f8e8cd2d31af32e84f15ae8
kiran <kiran@DESKTOP-6D0EAN5>
2020-08-31 23:38:34 +0530

    XSSF ADDED

--- src/com/payasia/generic/ExcelUtils.java
+++ src/com/payasia/generic/ExcelUtils.java
@@ -8,6 +8,7 @@ import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtils {
 	
@@ -19,7 +20,7 @@ public class ExcelUtils {
 		
 		String value=null;
 		try {
-			Workbook wb= WorkbookFactory.create(new FileInputStream(new File(filePath)));
+			XSSFWorkbook wb= (XSSFWorkbook) WorkbookFactory.create(new FileInputStream(new File(filePath)));
 			value=wb.getSheet(sheetName).getRow(rowNum).getCell(cellNum).getStringCellValue();
 		} catch (EncryptedDocumentException e) {
 			// TODO Auto-generated catch block
@@ -33,7 +34,10 @@ public class ExcelUtils {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 		return value;
 		
